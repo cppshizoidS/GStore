a9f4aafc97a0e4156cf7b294149a1fe6974e848a
Cppshizoid <alex.krivoschecov@gmail.com>
2022-11-01 09:10:16 -0700

    Add files via upload

---
+++ Models/Repository/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Data.Entity;
+
+namespace GameStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + HttpContext.Current.Server.MapPath("~/App_Data/GameStore.mdf")
+                    + ";Integrated Security=True")
+        {  }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
